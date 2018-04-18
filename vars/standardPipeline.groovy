4404812f620abd4928a0ac2b20df887f0e2fdad4
RavisankarCts <ravilink_2000@yahoo.com>
2018-04-18 13:30:54 +0200

    Changed to bat file

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -13,21 +13,21 @@ def call(body) {
 	        	checkout scm
 	        }
 	        stage ('Build') {
-	        	cmd "echo 'building ${config.projectName} ...'"
+	        	bat "echo 'building ${config.projectName} ...'"
 	        }
 	        stage ('Tests') {
 		        parallel 'static': {
-		            sh "echo 'shell scripts to run static tests...'"
+		            bat "echo 'shell scripts to run static tests...'"
 		        },
 		        'unit': {
-		            cmd "echo 'shell scripts to run unit tests...'"
+		            bat "echo 'shell scripts to run unit tests...'"
 		        },
 		        'integration': {
-		            cmd "echo 'shell scripts to run integration tests...'"
+		            bat "echo 'shell scripts to run integration tests...'"
 		        }
 	        }
 	      	stage ('Deploy') {
-	            cmd "echo 'deploying to server ${config.serverDomain}...'"
+	            bat "echo 'deploying to server ${config.serverDomain}...'"
 	      	}
 	    } catch (err) {
 	        currentBuild.result = 'FAILED'
