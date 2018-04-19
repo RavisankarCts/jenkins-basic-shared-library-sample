447f9f01c248f65a4ff9a9ee9da534dd9e1578a6
RavisankarCts <ravilink_2000@yahoo.com>
2018-04-19 14:34:12 +0200

    Update promote.groovy

--- vars/promote.groovy
+++ vars/promote.groovy
@@ -30,40 +30,6 @@ def call (stackname, hostname, version, stage) {
 		yaml = yaml.replaceAll("#KEY#","76008d58-42da-4a88-b4d3-7b81641e6cbe")
 		yaml = yaml.replaceAll("#HOST#", "http://${hostname}")
         writeFile file: "docker-compose-${stackname}.yaml" , text: yaml
-        sh "cat docker-compose-${stackname}.yaml"
-		// check if it already exists
-
-		String status = ""
-		try{
-			status = sh returnStdout: true, script: "${clientenvdle} docker service inspect -f {{.UpdateStatus.State}} ${stackname}_${appname}"
-		}catch(error){
-			status = "no such service"
-		}
-		sh "${clientenvdle} docker stack deploy -c docker-compose-${stackname}.yaml ${stackname}"
-        sh "echo 'service status is: ${status}'"
-		if(!status.equals("no such service")){
-			Integer retry = 0
-            sleep 15 //wait till service actually start updating
-            status = sh returnStdout: true, script: "${clientenvdle} docker service inspect -f {{.UpdateStatus.State}} ${stackname}_${appname}" //get status
-
-            while(!status.contains("completed")){
-				if(retry > 30){
-					error('took to long for the service to update')
-				}
-				sleep 10
-				status = sh returnStdout: true, script: "${clientenvdle} docker service inspect -f {{.UpdateStatus.State}} ${stackname}_${appname}"
-				retry++
-			}
-		}
-		status = ""
-		retry = 0
-		while(!status.contains("#httpcode:200")){
-			if(retry > 12){
-				error('took to long for the service to be deployed')
-			}
-			sleep 10
-			status = sh returnStdout: true, script: "curl -sL -w \"#httpcode:%{http_code}\\n\" \"http://${hostname}/health\""
-			retry++
-		}
+        
 	}
 }
